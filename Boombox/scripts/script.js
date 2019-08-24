08699e70e7cef16052489a7c86365f5a7389e39d
j-alizadeh <jalizade3d@yahoo.com>
2019-08-24 13:35:50 +0200

    speakers animation script

--- Boombox/scripts/script.js
+++ Boombox/scripts/script.js
@@ -12,7 +12,22 @@
 const Scene = require('Scene');
 const Animation = require('Animation');
 
+/*
+The following code finds the base_jnt object in the scene and stores it in a variable which we will use later to animate.
+
 const base = Scene.root.find('base_jnt');
+
+This is an opportunity to improve the performance of the script. In accessing the three objects in our scene we are calling Scene.root three times. If we store Scene.root in a variable we only have to call it once, and can use the variable as many times as we need it.
+*/
+const sceneRoot = Scene.root;
+
+const base = sceneRoot.find('base_jnt');
+
+//As with the base we need to access the speakers within the script, this time we want the speaker_left_jnt and speaker_right_jnt objects from the Scene Panel.
+const speakerLeft = sceneRoot.find('speaker_left_jnt');
+const speakerRight = sceneRoot.find('speaker_right_jnt');
+
+
 
 /*
 durationMilliseconds: The animation will last 0.4 seconds.
@@ -21,6 +36,13 @@ mirror: The animation will have returned to it's starting value by the time it h
 */
 const baseDriverParameters = {
     durationMilliseconds: 400,
+    loopCount: Infinity,
+    mirror: true
+};
+
+//for speaker animation
+const speakerDriverParameters = {
+    durationMilliseconds: 200,
     loopCount: Infinity,
     mirror: true
 };
@@ -34,6 +56,10 @@ The time driver also needs to be instructed to start.
 const baseDriver = Animation.timeDriver(baseDriverParameters); // Existing code
 baseDriver.start();
 
+//also for speaker animation
+const speakerDriver = Animation.timeDriver(speakerDriverParameters);
+speakerDriver.start();
+
 
 /*
 The sampler allows you to specify the beginning and end values of the animation as well as the easing function applied to it, altering the rate of change.
@@ -44,10 +70,23 @@ const baseSampler = Animation.samplers.easeInQuint(0.9,1);
 
 
 /*
+For the sampler we'll be using a different easing function and values.
+
+The values are smaller than the ones we used for the base because the speakers themselves are smaller objects. An ease out function means the animation will start fast but end slower.
+*/
+const speakerSampler = Animation.samplers.easeOutElastic(0.7,0.85);
+
+
+
+/*
 The animation is created by combining the driver and sampler.
 The animate() method of the animation module returns a ScalarSignal that is changing between 0.9 and 1 every 0.4 seconds according to the easing function used.
 */
 const baseAnimation = Animation.animate(baseDriver,baseSampler);
+
+
+//With the driver and sampler created we can now create the animation.
+const speakerAnimation = Animation.animate(speakerDriver,speakerSampler);
 
 
 //To access the scale of the base we need to first access the transform. The transform represents the object's transformation (position, scale and rotation) in it's local coordinate system.
@@ -58,3 +97,21 @@ const baseTransform = base.transform;
 baseTransform.scaleX = baseAnimation;
 baseTransform.scaleY = baseAnimation;
 baseTransform.scaleZ = baseAnimation;
+
+
+/*
+We can now apply the new animation to both of the speaker objects, the animation will be used to change their scale.
+
+The transforms of the speakers are accessed before using the scale methods to bind the speakerAnimation signal to them. This means both speakers will animate in the same way.
+*/
+const speakerLeftTransform = speakerLeft.transform;
+
+speakerLeftTransform.scaleX = speakerAnimation;
+speakerLeftTransform.scaleY = speakerAnimation;
+speakerLeftTransform.scaleZ = speakerAnimation;
+
+const speakerRightTransform = speakerRight.transform;
+
+speakerRightTransform.scaleX = speakerAnimation;
+speakerRightTransform.scaleY = speakerAnimation;
+speakerRightTransform.scaleZ = speakerAnimation;
