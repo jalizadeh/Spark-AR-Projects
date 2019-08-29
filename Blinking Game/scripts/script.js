cc2be30dff5175eed12ca13a945c3143db123564
j-alizadeh <jalizade3d@yahoo.com>
2019-08-29 22:55:54 +0200

    Pathc - Counter added

---
+++ Blinking Game/scripts/script.js
@@ -0,0 +1,21 @@
+//==============================================================================
+// Welcome to scripting in Spark AR Studio! Helpful links:
+//
+// Scripting Basics - https://fb.me/spark-scripting-basics
+// Reactive Programming - https://fb.me/spark-reactive-programming
+// Scripting Object Reference - https://fb.me/spark-scripting-reference
+// Changelogs - https://fb.me/spark-changelog
+//==============================================================================
+
+// How to load in modules
+const Diagnostics = require('Diagnostics');
+const Scene = require('Scene');
+
+// How to access scene objects (uncomment line below to activate)
+// const directionalLight = Scene.root.find('directionalLight0');
+
+// How to access class properties (uncomment line below to activate)
+// const directionalLightIntensity = directionalLight.intensity;
+
+// How to log messages to the console (uncomment line below to activate)
+// Diagnostics.log('Console message logged from the script.');
